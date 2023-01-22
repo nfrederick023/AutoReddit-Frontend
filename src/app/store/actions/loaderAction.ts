c6c0b5fd22539dec15d99cc17378a475ac4ec1b1
nfrederick023 <nfrederick023@gmail.com>
2023-01-21 23:37:55 -0600

    part 2

--- src/app/store/actions/loaderAction.ts
+++ src/app/store/actions/loaderAction.ts
@@ -1,7 +1,7 @@
-import { LoaderState } from '../../common/interfaces/loader';
+import { LoaderState } from '../../utils/types';
 import { createAction } from '@reduxjs/toolkit';
 
-export const displayLoader = createAction('dispatchLoader', ( isLoading: boolean) => {
-  const payload: LoaderState = {isLoading};
-  return {payload};
+export const displayLoader = createAction('dispatchLoader', (isLoading: boolean) => {
+    const payload: LoaderState = { isLoading };
+    return { payload };
 });
